c95c1215d57b5467c00a2f2fd7a5ff9c54d8901e
Simon Constans <simonc@linux.com>
2015-10-21 19:16:01 +0200

    Update 2015-10-21T17:15:55.301Z

---
+++ js/menu.js
@@ -0,0 +1,29 @@
+(function() {
+  var selectMenuItem, thumbnail;
+
+  if ($("#portfolio").length > 0) {
+    thumbnail = $('.thumbnail');
+    selectMenuItem = function() {
+      var anchor, element;
+      $(".menu a").removeClass('selected');
+      anchor = window.location.hash.substring(1);
+      element = $(".menu a[href='/portfolio.html#" + anchor + "']");
+      if (element.length > 0) {
+        element.addClass('selected');
+      }
+      if (anchor === "") {
+        return thumbnail.show();
+      } else {
+        thumbnail.filter("[data-category='" + anchor + "']").show();
+        return thumbnail.not("[data-category='" + anchor + "']").hide();
+      }
+    };
+    window.onhashchange = function() {
+      return selectMenuItem();
+    };
+    selectMenuItem();
+  }
+
+}).call(this);
+
+ //# sourceMappingURL=menu.js.map
